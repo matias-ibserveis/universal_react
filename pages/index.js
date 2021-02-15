0ee7b78998475a443a2b113a67a2035f5e7d7f7e
matias <matias@ibserveis.com>
2021-02-15 20:06:04 +0100

    css style en index

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,29 @@ const Index = () => (
     </Link>
 
     <p>Welcome to the home page</p>
-    
+
+
+    <style jsx>{`
+        a {
+         color: blue;
+         text-decoration: underline;
+        }
+    `}
+    </style>
+    <style global jsx>{`
+      a {
+        color: red;
+        margin: 0;
+        font-size: 110%;
+        background: #f0f0f0;
+      }
+    `}</style>
+
   </div>
+
+  
 )
+
+
 
 export default Index
