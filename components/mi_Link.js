7c5dee936eb8a323f018152b0896f61e0562b9e3
matias <matias@ibserveis.com>
2021-02-16 18:39:13 +0100

    Link customizado OK

--- components/mi_Link.js
+++ components/mi_Link.js
@@ -1,28 +1,46 @@
 
+// Customize CSS:
+// https://material-ui.com/customization/components/
+// https://codeburst.io/my-journey-to-make-styling-with-material-ui-right-6a44f7c68113
+
+
 import Link from 'next/link'
+import { Typography} from '@material-ui/core'
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles({
+  enlace: {
+    color: 'green',
+    fontFamily: 'times new roman',
+    textDecoration: 'underline',
+    fontSize: '1.5rem',
+    '&:hover': {
+      color: 'red',
+    },
+  }
+})
 
 const mi_Link = (props) => {
   const {texto, enlace} = props
   const cadena_enlace= `/${enlace}`
 
+  const classes = useStyles()
+
   return (
   <div>
  
-    <Link href={cadena_enlace}>
-      <a>{texto}</a>
-    </Link>
+    <Link href={cadena_enlace} >
+    <Typography 
+        variant="h3"
+        color="secondary"
+        className={classes.enlace}>  
+        {texto}
+    </Typography>
 
-    <style global jsx>{`
-        a {
-         color: green;
-         text-decoration: underline;
-        }
-    `}
-    </style>
+    </Link>
    
   </div>
 )
-
 }
 
 export default mi_Link
--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,19 @@
 
 import Mi_Link from '../components/Mi_Link.js'
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles({
+   media: {
+    height: 100,
+    width: '50%'
+  },
+})
 
 const About = () => {
 
   const texto_home = 'Go to home'
   const enlace_home = ''
+  const classes = useStyles()
 
   return (
   <div>
@@ -17,7 +26,7 @@ const About = () => {
     </Mi_Link>
 
     <p>A Javascript programmer</p>
-    <img src="imagenes/javascript-logo.png" alt="logo" height="200px" />
+    <img  src="imagenes/javascript-logo.png" alt="logo"  className={classes.media} / >
   </div>
 )
   }
