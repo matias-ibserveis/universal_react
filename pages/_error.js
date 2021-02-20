5ddaeed3206df02c49532028ef9cac4f4c15f54f
matias <matias@ibserveis.com>
2021-02-20 20:11:36 +0100

    Lesson9_custom_error_page

--- pages/_error.js
+++ pages/_error.js
@@ -1,10 +1,13 @@
 import Layout from "./components/Layout";
 
-export default ({ statusCode }) => (
+export default function ErrorPage({ statusCode }) {
+  
+  return(
   <Layout title="Error!!!">
     {statusCode
       ? `Could not load your user data: Status Code ${statusCode}`
       : `Couldn't get that page, sorry!`}
   </Layout>
+  )
   
-);
+  }
